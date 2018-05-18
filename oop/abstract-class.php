c799422c197385c938373941c49df9b2f0602647
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-05-18 15:30:11 +0600

    abstract class

--- oop/abstract-class.php
+++ oop/abstract-class.php
@@ -1,16 +1,36 @@
 <?php
+
 /*
- * Abstract class cannot be instantiated
+ * Abstract class cannot be instantiated, abstract class can be only extended
+ * Abstract class's method cannot access directly
  *Different between abstract class and interface
  * 1. An abstract class does not provide full abstraction but an interface provide full abstraction.
  * This means an abstract classes can have non abstract member too
- * 2.
- * */
-abstract class A {
+ * 2. In interface we cannot define of body of a function but in interface class we can define body of the function of on abstract method
+ * 
+ */
 
-    public function my_function(){
-        echo "";
+abstract class Car
+{
+    protected $name;
+
+    public function getName()
+    {
+        echo $this->name;
+    }
+
+    abstract public function setName($name);
+}
+
+class BMW extends Car
+{
+    public function setName($name)
+    {
+        $this->name = $name;
     }
 }
 
-//$obj = new A();
+
+$obj = new BMW();
+//$obj->setName("BMD");
+$obj->getName();
