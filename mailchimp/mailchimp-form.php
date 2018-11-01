58b5ee33e98f3ef4499b89ecd97c1c581745edb8
iman <iman@bitmascot.com>
2018-11-01 08:12:10 +0600

    mailchimp

--- mailchimp/mailchimp-form.php
+++ mailchimp/mailchimp-form.php
@@ -1,24 +1,31 @@
-<form action="upload_mailchimp_subscriber"
-      method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form"
-      class="validate mailchimp">
-    <div id="mc_embed_signup_scroll">
-        <div class="mc-field-group newsletter-input">
-            <div>
-                <input type="email" value="" name="email" class="email" id="email"
-                       placeholder="Enter Address *" required="" aria-required="true">
-                <span class="newsletter_mail_error"></span>
-            </div>
-            <div>
-                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input
-                        type="text" name="b_3eedf62523fb63e16a809814c_78cdb59ddb" tabindex="-1"
-                        value=""></div>
-                <input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe"
-                       class="button submit-newsletter btn  btn-block">
+<div class="newsletter-block">
+    <h4>Newsletter Signup</h4>
+    <!-- Begin MailChimp Signup Form -->
+    <link href="https://cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet"
+          type="text/css">
+    <form action="upload_mailchimp_subscriber"
+          method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form"
+          class="validate mailchimp">
+        <div id="mc_embed_signup_scroll">
+            <div class="mc-field-group newsletter-input">
+                <div>
+                    <input type="email" value="" name="email" class="email" id="email"
+                           placeholder="Enter Address *" required="" aria-required="true">
+                    <span class="newsletter_mail_error"></span>
+                </div>
+                <div>
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input
+                            type="text" name="b_3eedf62523fb63e16a809814c_78cdb59ddb" tabindex="-1"
+                            value=""></div>
+                    <input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe"
+                           class="button submit-newsletter btn  btn-block">
+                </div>
             </div>
         </div>
-    </div>
-    <div class="loader" style="display: none;"><img
-            src="https://www.eventbookings.com/wp-content/themes/webalive/images/ajax-loader.gif"/>
-    </div>
-</form>
-<div class="message"></div>
+        <div class="loader" style="display: none;"><img
+                src="https://www.eventbookings.com/wp-content/themes/webalive/images/ajax-loader.gif"/>
+        </div>
+    </form>
+    <span class="message" style="display: none;"> </span>
+    <!--End mc_embed_signup-->
+</div>
