ebe2c50d1c7b15690e8a6687337b1c0d54db1977
iman <iman@bitmascot.com>
2018-11-05 08:41:53 +0600

    addHttps modified

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function addHttps($url) {
     $return_url = $url;
-    if (isset($_SERVER['HTTPS'])  && !empty($_SERVER['HTTPS'])){
+    if (isset($_SERVER['HTTPS'])  && $_SERVER['HTTPS'] != 'off'){
         $return_url = preg_replace("/http:/", "https:", $url);
     }
     return $return_url;
