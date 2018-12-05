950ef842c924e88cda63f0d02281a1697ddff394
iman <iman@bitmascot.com>
2018-12-05 17:49:13 +0600

    secure pay

---
+++ payment/secure_pay/payment_iframe.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/components/core-min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/components/sha1-min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+    <script>
+        $(function() {
+            var merchantId = $("#merchantID").val();;
+            var password =$("#merchantPassword").val();
+            var txnType = "0";
+            var primaryRef = $("#primary_ref").val();//"Test Reference";
+            var amount = $("#amount").val();
+
+            var timestamp = moment.utc().format("YYYYMMDDHHMMSS");
+            var fingerprint = CryptoJS.enc.Hex.stringify(
+                CryptoJS.SHA1([merchantId, password, txnType, primaryRef, amount, timestamp].join("|"))
+            );
+            $('input[name="merchant_id"]').val(merchantId);
+            $('input[name="txn_type"]').val(txnType);
+            $('input[name="fp_timestamp"]').val(timestamp);
+            $('input[name="fingerprint"]').val(fingerprint);
+            $("form").submit();
+        });
+    </script>
+</head>
+<body>
+<form action="https://test.payment.securepay.com.au/secureframe/invoice" method="post">
+    <input type="hidden" name="merchantID" id="merchantID" value="ABC0001">
+    <input type="hidden" name="merchantPassword" id="merchantPassword" value="abc123">
+
+    <input type="hidden" name="bill_name" value="transact">
+    <input type="hidden" name="merchant_id">
+    <input type="hidden" name="txn_type">
+    <input id="primary_ref"  type="hidden" name="primary_ref" value="4444333322221111">
+    <input id="amount" type="hidden" name="amount"  value="2600">
+    <input type="hidden" name="fp_timestamp">
+    <input type="hidden" name="fingerprint">
+    <input type="hidden" name="currency" value="AUD">
+    <input type="hidden" name="return_url" value="http://facebookshop.info/checkouts/processOrder">
+    <input type="hidden" name="callback_url" value="http://facebookshop.info//universities/securepayCallbackUrl">
+    <input type="hidden" name="cancel_url" value="http://facebookshop.info/my-account">
+
+    <input type="hidden" name="return_url_text" value="Continue...">
+    <input type="hidden" name="return_url_target" value="parent">
+    <input type="hidden" name="title" value="Payment Page">
+    <input type="hidden" name="primary_ref_name" value="Order Number">
+    <input type="hidden" name="template" value="responsive">
+    <input type="hidden" name="display_receipt" value="no">
+    <input type="hidden" name="confirmation" value="no">
+    <input type="hidden" name="page_style_url" value="http://facebookshop.info/css/styles/secure_frame.css">
+</form>
+</body>
+</html>
---
+++ payment/secure_pay/securepay.php
@@ -0,0 +1,93 @@
+<?php
+ini_set('max_execution_time', 0);
+
+$date = new DateTime();
+
+$xml="<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+$xml.="<SecurePayMessage>";
+    $xml.=  "<MessageInfo>";
+        $xml.="<messageID>8af793f9af34bea0cf40f5fb750f64</messageID>";
+        $xml.="<messageTimestamp>" . $date->getTimestamp() . "</messageTimestamp>";
+        $xml.="<timeoutValue>60</timeoutValue>";
+        $xml.="<apiVersion>xml-4.2</apiVersion>";
+    $xml.="</MessageInfo>";
+
+    $xml.="<MerchantInfo>";
+        $xml.="<merchantID>ABC0001</merchantID>";
+        $xml.="<password>abc123</password>";
+    $xml.="</MerchantInfo>";
+
+    $xml.="<RequestType>Payment</RequestType>";
+    $xml.="<Payment>";
+        $xml.="<TxnList count='1'>";
+            $xml.="<Txn ID='1'>";
+                $xml.="<txnType>0</txnType>";
+                $xml.="<txnSource>23</txnSource>";
+                $xml.="<amount>2600</amount>";
+                $xml.="<recurring>No</recurring>";
+                $xml.="<currency>AUD</currency>";
+                $xml.="<purchaseOrderNo>125</purchaseOrderNo>";
+
+                $xml.="<CreditCardInfo>";
+                    $xml.= "<cardType>3</cardType>";
+                    $xml.= "<cardNumber>4444333322221111</cardNumber>";
+                    $xml.= "<expiryDate> 08/23</expiryDate>";
+                    $xml.= "<cvv>123</cvv>";
+                $xml.="</CreditCardInfo>";
+
+            $xml.="</Txn>";
+        $xml.="</TxnList>";
+    $xml.="</Payment>";
+$xml.="</SecurePayMessage>";
+
+
+
+$url = "https://test.api.securepay.com.au/xmlapi/payment"; //test mode
+//$url = "https://payment.securepay.com.au/secureframe/payment";  //live mode
+
+$ch = curl_init();
+curl_setopt( $ch, CURLOPT_URL, $url);
+curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+curl_setopt( $ch, CURLOPT_POST, true );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $xml );
+$result = curl_exec($ch);
+curl_close($ch);
+
+
+$xml = simplexml_load_string($result);
+$json = json_encode($xml);
+$xml_data= json_decode($json,TRUE);
+
+$data['transactionDetails'] = array(
+    'order_id' => 10,
+    'merchantID' => $xml_data['MerchantInfo']['merchantID'],
+    'txnID' => $xml_data['Payment']['TxnList']['Txn']['txnID'],
+    'txnType' => $xml_data['Payment']['TxnList']['Txn']['txnType'],
+    'txnSource' => $xml_data['Payment']['TxnList']['Txn']['txnSource'],
+    'pan' => $xml_data['Payment']['TxnList']['Txn']['CreditCardInfo']['pan'],
+    'expiryDate' => $xml_data['Payment']['TxnList']['Txn']['CreditCardInfo']['expiryDate'],
+    'cardType' => $xml_data['Payment']['TxnList']['Txn']['CreditCardInfo']['cardType'],
+    'cardDescription' => $xml_data['Payment']['TxnList']['Txn']['CreditCardInfo']['cardDescription'],
+    'amount' => $xml_data['Payment']['TxnList']['Txn']['amount'],
+    'currency' => $xml_data['Payment']['TxnList']['Txn']['currency'],
+    'approved' => $xml_data['Payment']['TxnList']['Txn']['approved'],
+    'responseCode' => $xml_data['Payment']['TxnList']['Txn']['responseCode'],
+    'responseText' => $xml_data['Payment']['TxnList']['Txn']['responseText'],
+    'settlementDate' => $xml_data['Payment']['TxnList']['Txn']['settlementDate'],
+    'statusCode' => $xml_data['Status']['statusCode'],
+    'statusDescription' => $xml_data['Status']['statusDescription'],
+    'thinlinkResponseCode' => $xml_data['Payment']['TxnList']['Txn']['thinlinkResponseCode'],
+    'thinlinkResponseText' => $xml_data['Payment']['TxnList']['Txn']['thinlinkResponseText'],
+    'thinlinkEventStatusCode' => $xml_data['Payment']['TxnList']['Txn']['thinlinkEventStatusCode'],
+    'thinlinkEventStatusText' => $xml_data['Payment']['TxnList']['Txn']['thinlinkEventStatusText']
+);
+
+//print_r($xml_data);
+print_r($data);
+
+
+
+
+
