5337b389ea6143dc95614dacdc2833b5c291cbd9
iman <iman@bitmascot.com>
2018-12-05 16:22:39 +0600

    secure pay debug

--- payment/secure_payment.php
+++ payment/secure_payment.php
@@ -330,11 +330,30 @@ class securePay
         }
     }
 
-    function log($data, $file_name)
+    function log($log, $file_name = '')
     {
-        $new_data = date('Y-m-d H:i:s') . " ";
-        $new_data .= json_encode($data) . "\n\n";
-        error_log($new_data, 3, 'logs/' . $file_name . '.log');
+        if (empty($file_name)) {
+            $file_name = 'debug';
+        }
+
+        $file_name = $file_name . '.log';
+        $folder = 'logs';
+
+        if(!file_exists('logs')){
+            mkdir($folder, 755);
+        }
+
+        $file_path = $folder.'/' . $file_name;
+
+        if (is_array($log) || is_object($log)) {
+            $log_data = print_r($log, true);
+        } else {
+            $log_data = $log;
+        }
+
+        $log_data = date('Y-m-d H:i:s') . " Debug: \n" . $log_data."\n\n";
+
+        error_log($log_data, 3, $file_path);
     }
 
 }
