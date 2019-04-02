99af969b3632557a48cbc82b4e44c75280817607
iman <iman@bitmascot.com>
2019-04-02 15:18:40 +0600

    secure frame style

--- payment/payway/trusted_frame/host_page.php
+++ payment/payway/trusted_frame/host_page.php
@@ -10,11 +10,34 @@
 </script>
 <script type="text/javascript">
     var submit = document.getElementById('payway-cc-submit');
+
+    var createdCallback = function( err, frame ) {
+        if ( !err ) {
+            console.log('Yes');
+        }
+    };
+
+    var style = {
+        'div.payway-card' : { 'background-color': '#404040',
+            'border-radius': '0em' },
+        '.payway-card label' : { 'color' : 'white' },
+        '.payway-card legend': { 'color' : 'white' },
+        '.payway-card input' : { 'color' : '#202020' },
+        '.payway-card select': { 'color' : '#202020' }
+    };
+
     payway.createCreditCardFrame({
         publishableApiKey: 'T11915_PUB_gjjg685bbrqkvwzb2jzw8ye4wu8xvfigkzfqmqrshzy28y67fp765pqjangi',
         onValid: function() { submit.disabled = false; },
-        onInvalid: function() { submit.disabled = true; }
-    });
+        onInvalid: function() { submit.disabled = true; },
+        style: style,
+        layout: 'narrow'
+        },
+        createdCallback
+    );
+//
+
+
 </script>
 </body>
 </html>
