e2166772c3ac09414224d4ba1cc20b66cfb7e3c4
iman <iman@bitmascot.com>
2019-04-22 12:15:41 +0600

    quick web secure token

--- payment/quickweb/secure_token_request2.php
+++ payment/quickweb/secure_token_request2.php
@@ -4,14 +4,8 @@
  * https://quickstream.westpac.com.au/docs/quickweb/#parameters-for-secure-token-request
  */
 
-
-//define('BASE_URL', 'https://quickweb.westpac.com.au');
-
-define('MODE', 'LIVE');
-//define('BASE_URL', 'https://quickstream.support.qvalent.com');
-define("USERNAME", "");
-define("PASSWORD", "");
-define("supplierBusinessCode", "");
+//$base_url = url('');
+$base_url = 'https://nsw-records.webmascot.com.au';
 
 $postFields = array(
     "username" => "",
@@ -19,57 +13,59 @@ $postFields = array(
     "supplierBusinessCode" => "",
     "connectionType" => "QUICKWEB",
     "product" => "QUICKWEB",
-    "customerReferenceNumber" => "CUSTOMER1"
+    "principalAmount"=> 12,
+    "paymentReference"=> 'nsw-'.time(),
+    "customerReferenceNumber" => "CUSTOMER1",
+    "returnUrl"=> $base_url.'/checkout/returnUrl',
+    "serverReturnUrl"=> $base_url.'/api/checkout/serverReturnUrl',
+    "errorEmailToAddress"=> 'iman@bitmascot.com',
 );
+
+echo '<pre>';
+print_r($postFields);
+echo '</pre>';
 
 $postFieldsString = http_build_query( $postFields );
 $curlHandle = curl_init();
 
 curl_setopt( $curlHandle, CURLOPT_URL, "https://ws.support.qvalent.com/services/quickweb/CommunityTokenRequestServlet"); //Test
-//curl_setopt( $curlHandle, CURLOPT_URL, "https://ws.qvalent.com/services/quickweb/CommunityTokenRequestServlet" ); //LIVE
 
-echo '<pre>';
-print_r($postFields);
-echo '</pre>';
 if( array_key_exists( "user", $_POST ) &&
     array_key_exists( "pwd", $_POST ) )
 {
     curl_setopt( $curlHandle, CURLOPT_PROXY, "proxy.yourdomain.com.au:8080" );
     curl_setopt( $curlHandle, CURLOPT_PROXYUSERPWD, $_POST["user"].":".$_POST["pwd"] );
 }
+
 curl_setopt( $curlHandle, CURLOPT_POST, count( $postFields ) );
 curl_setopt( $curlHandle, CURLOPT_POSTFIELDS, $postFieldsString );
 curl_setopt( $curlHandle, CURLOPT_RETURNTRANSFER, 1 );
-curl_setopt( $curlHandle, CURLOPT_CAINFO, "PCA-3G5.pem" );
+//curl_setopt( $curlHandle, CURLOPT_CAINFO, "PCA-3G5.pem" );
 curl_setopt( $curlHandle, CURLINFO_HEADER_OUT, 1 );
 $result = curl_exec( $curlHandle );
 
-echo '<pre>';
-print_r($result);
-echo '</pre>';
+$token = str_replace('token=', '', $result);
 
 ?>
-    <html>
-    <head>
-    </head>
-    <body>
     <p> Header: <?php echo curl_getinfo( $curlHandle, CURLINFO_HEADER_OUT ); ?> </p>
-    <?php
-    if( curl_errno( $curlHandle ) )
-    {
-        ?>
-        <p> Error: <?php echo curl_error( $curlHandle ); ?> </p>
-        <?php
-    }
-    else
-    {
-        ?>
-        <p> Token: <?php echo $result; ?> </p>
-        <?php
-    }
-    ?>
-    </body>
-    </html>
 <?php
-curl_close( $curlHandle );
+if( curl_errno( $curlHandle ) )
+{
+    ?>
+    <p> Error: <?php echo curl_error( $curlHandle ); ?> </p>
+    <?php
+}
+else
+{
+    ?>
+    <p> Token: <?php echo $result; ?> </p>
+    <?php
+}
+echo 'new token: '.$token;
 ?>
+    <form action="https://quickweb.support.qvalent.com/OnlinePaymentServlet3" method="POST">
+        <input type="hidden" name="token" value="<?php echo $token ?>"/>
+        <input type="hidden" name="communityCode" value=""/>
+        <input type="submit" value="Make Payment"/>
+    </form>
+<?php
