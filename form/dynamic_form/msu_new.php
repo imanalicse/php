cb51b66b813374f0ee04c256c06d8a794bf12563
Iman <imanali.cse@gmail.com>
2021-04-09 20:04:59 +0600

    dynamic form

---
+++ form/dynamic_form/msu_new.php
@@ -0,0 +1,329 @@
+<html>
+<head>
+	<title></title>
+	<style type="text/css">.form-wrapper {
+     width: 100%;
+     }
+.inline-row.top-row-form {
+    padding-top: 20px;
+}
+.yes-no-box{
+   padding-top:18px;
+}
+.radio-button{
+    word-spacing: 2px;
+}
+.radio-button label {
+    margin-right: 11px;
+    font-weight: 500!important;
+    font-size: 13px!important;
+}
+.survey-form-footer.continue-btn-wrap {
+    padding-top: 21px;
+    padding-left: 0px;
+}
+.survey-form-header {
+    background-color:#2a94be;
+    text-align: center;
+    padding:10px 30px;
+    display: flex;
+    align-items: center;
+    border: 1px solid #d0d0d0;
+    border-bottom: 0px;
+}
+.survey-brand-logo {
+    max-width: 20%;
+    flex-basis: 20%;
+}
+.survey-brand-title {
+    flex: auto;
+    margin-left: 20px;
+    padding-left: 16%;
+    text-align: center;
+}
+.survey-form-header h1 {
+    margin: 0;
+    color: #fff;
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 30px;
+    text-align: left;
+    text-transform: uppercase;
+}
+.survey-form-body{
+    padding: 15px 30px;
+    border: 1px solid #d0d0d0;
+    border-top:0px;
+}
+.survey-form-footer {
+    padding: 15px 30px;
+    border: 0px solid #d0d0d0;
+    border-bottom: 0px solid #d0d0d0;
+    border-top: 0px;
+    padding-top:0px;
+   text-align:left;
+   margin-top:-15px;
+}
+.form-wrapper .content-box {
+    margin-bottom: 20px;
+    padding-top:15px;
+}
+.email-popup-model label {
+    color: #000;
+    font-size: 14px;
+    margin-bottom: 6px;
+    line-height: 23px;
+    font-weight: 700;
+}
+.survey-form-body .required {
+	color:#000000
+}
+.survey-form-body .inline-row {
+    margin: 0;
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-justify-content: space-between;
+    justify-content: space-between;
+    margin-bottom:10px;
+}
+.survey-form-body .inline-row .from-row {
+    width: calc(50% - 15px);
+    max-width: 50%;
+    margin-bottom:10px;
+}
+.survey-form-body .from-row .label {
+    font-size: 13px;
+    font-weight: 700;
+    color: #202020;
+    padding: 0;
+    line-height:22px;
+}
+.form-control:disabled, .form-control[readonly] {
+    background-color: #f7f7f7;
+    color: #0d77a1;
+    line-height: 32px!important;
+    height: 40px!important;
+}
+.survey-form-footer {
+    text-align: left;
+    padding-bottom: 30px;
+}
+.inline-content-box{
+    font-weight: 600;
+    padding-top: 8px;
+    font-size: 19px;
+    padding-bottom: 7px;
+}
+.cngratulation-text{
+   line-height:27px!important;
+   font-weight:500;
+   color: #20496a;
+}
+label.plz-content {
+    font-weight: 600;
+    margin-top: 15px;
+    font-size: 18px;
+}
+.from-rowb{
+  width:100%;
+}
+.count-number{
+    position: absolute;
+    right: 0;
+    font-size: 11px;
+    color: #716f6f;
+    top: -17px;
+    font-weight: normal;
+}
+.texarea-box{
+   position:relative;
+}
+.csp-popup .error {
+  padding:0px 15px;
+  text-align: left;
+}
+.csp-popup label.error {
+    margin-top: -19px;
+    display: block;
+    padding: 0;
+    padding-left: 8px;
+    padding-right: 11px;
+    font-size: 12px;
+    color: red;
+    text-align: left;
+    font-weight: normal;
+}
+.csp-popup .texarea-box label.error {
+    margin-top: -10px;
+}
+label.cngratulation-text {
+    font-weight: 600;
+    color: #002f55;
+    line-height: 24px!important;
+    font-size: 14px;
+}
+/*Responsive start*/
+@media (max-width:767px) {
+ .survey-form-body .inline-row {
+    display: block;
+    display: initial;
+}
+.survey-form-body .inline-row .from-row {
+    width: calc(100% - 0px);
+    max-width: 100%;
+    margin-bottom: 10px;
+}
+.survey-form-body {
+    padding: 15px 15px;
+}
+}
+@media (max-width:500px) {
+.survey-brand-logo {
+    max-width: 100%;
+    flex-basis: 100%;
+}
+.survey-form-header {
+    padding: 8px 15px;
+    display: block;
+}
+.survey-brand-title {
+    flex: initial;
+    margin-left: 0;
+    padding-left: 0%;
+    text-align: center;
+    padding-top: 10px;
+}
+}
+/*Responsive end*/
+	</style>
+	<style type="text/css">
+	</style>
+	<style type="text/css">
+	</style>
+	<style type="text/css">
+	</style>
+
+    <script
+            src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous"></script>
+    <script src="assets/jquery.validate.js"></script>
+    <script src="assets/custom.js"></script>
+
+</head>
+<body data-gr-ext-installed="" data-new-gr-c-s-check-loaded="14.1000.0" data-new-gr-c-s-loaded="14.1000.0">
+<div style="display: none;">&nbsp;</div>
+
+<div style="display: none;">&nbsp;</div>
+
+<div style="display: none;">&nbsp;</div>
+
+<div class="modal-dialog email-popup-model view-popupimage csp-popup">
+<div class="modal-content portrait-content">
+<div class="modal-body">
+<form action="/products/surveyProcessForm" id="student_survey_form" method="post" novalidate="">
+<div class="form-wrapper from-serv-box">
+<div class="survey-form-header">
+<div class="survey-brand-logo"><img alt="logo" src="/img/servey/msu/servey-logo5.png" /></div>
+
+<div class="survey-brand-title">
+<h1>MICHIGAN STATE UNIVERSITY<br />
+2021 SPRING COMMENCEMENT</h1>
+</div>
+</div>
+
+<div class="survey-form-body"><input name="data[StudentSurvey][extra][student_consent_no_checkout_for]" type="hidden" value="No" /> <input name="data[StudentSurvey][consent_type]" type="hidden" value="physical-presence" /> <input id="has_submit_confimation" name="has_submit_confimation" type="hidden" value="Are you sure to proceed?" />
+<div class="yes-no-box">
+<div class="inline-row">
+<div class="from-row-alumi"><span style="font-size: 14px;"><b>I will attend the ceremony:</b></span>
+
+<div class="radio-button"><span><input class="required" id="news_yes" name="data[StudentSurvey][student_consent]" type="radio" value="1" /> <label for="news_yes">Yes</label> </span> <span><input class="required" id="news_no" name="data[StudentSurvey][student_consent]" type="radio" value="0" /> <label for="news_no">No</label></span></div>
+</div>
+</div>
+</div>
+
+<div class="content-box block-hidenbox">
+<div class="inline-content-box">Future Information</div>
+
+<div class="inline-row">
+<div class="from-row"><label class="label">Phone</label> <input class="input form-control required" name="data[StudentSurvey][extra][phone]" placeholder="" type="text" value="" /></div>
+
+<div class="from-row"><label class="label">Non-MSU Email </label> <input class="input form-control required" name="data[StudentSurvey][extra][non_msu_email]" type="email" value="" /></div>
+</div>
+
+<div class="inline-content-box">Permanent Mailing Address:</div>
+
+<div class="inline-row">
+<div class="from-row"><label class="label">Address Line 1</label> <input class="input form-control required" name="data[StudentSurvey][extra][address_line_1]" placeholder="" type="text" value="" /></div>
+
+<div class="from-row"><label class="label">Address Line 2</label> <input class="input form-control" name="data[StudentSurvey][extra][address_line_2]" type="text" value="" /></div>
+</div>
+
+<div class="inline-row">
+<div class="from-row"><label class="label">City</label> <input class="input form-control required" name="data[StudentSurvey][extra][city]" type="text" value="" /></div>
+
+<div class="from-row"><label class="label">State</label> <input class="input form-control required" name="data[StudentSurvey][extra][state]" type="text" value="" /></div>
+</div>
+
+<div class="inline-row">
+<div class="from-row"><label class="label">Postcode</label> <input class="input form-control required" name="data[StudentSurvey][extra][postcode]" type="text" value="" /></div>
+
+<div class="from-row"><label class="label">Country</label> <input class="input form-control required" name="data[StudentSurvey][extra][country]" type="text" value="" /></div>
+</div>
+
+<div class="inline-row">
+<div class="from-row-alumi"><label class="label">Would you like to learn more about opportunities for Spartan alumni?</label>
+
+<div class="radio-button"><span><input class="required" id="opportun_yes" name="data[StudentSurvey][extra][opportunity_for_spartan_alumni]" type="radio" value="Yes" /> <label for="opportun_yes">Yes</label> </span> <span><input id="opportun_no" name="data[StudentSurvey][extra][opportunity_for_spartan_alumni]" type="radio" value="No" /> <label for="opportun_no">No</label></span></div>
+</div>
+</div>
+
+<div class="inline-row" id="after_graguation_plan_wrapper">
+<div class="from-row-alumi"><label class="label">Please tell us about your plans about after graduation</label>
+
+<div class="radio-button js-input-group-wrapper">
+<div class="js-input-wrapper" data-conditional-value="I have a job offer in">
+<input name="data[StudentSurvey][extra][after_graduation_plan][input_type_radio_selected_index]" class="input_type_radio_selected_index" type="hidden" value="1" />
+<input class="required" id="after_graguation_plan_radio1" name="data[StudentSurvey][extra][after_graduation_plan][0][field_value]" type="radio" value="I have a job offer in" /> <label for="after_graguation_plan_radio1">I have a job offer in</label> 
+<div class="js-conditional-display js-input-wrapper" style="display:none">
+<select class="required" name="data[StudentSurvey][extra][after_graduation_plan][0][child_value]"><option value="">Where is the location of your job?</option><option value="Michigan">Michigan</option><option value="Alabama">Alabama</option><option value="Alaska">Alaska</option><option value="Arizona">Arizona</option><option value="Arkansas">Arkansas</option><option value="California">California</option><option value="Colorado">Colorado</option><option value="Connecticut">Connecticut</option><option value="Delaware">Delaware</option><option value="District of Columbia">District of Columbia</option><option value="Florida">Florida</option><option value="Georgia">Georgia</option><option value="Hawaii">Hawaii</option><option value="Idaho">Idaho</option><option value="Illinois">Illinois</option><option value="Indiana">Indiana</option><option value="Iowa">Iowa</option><option value="Kansas">Kansas</option><option value="Kentucky">Kentucky</option><option value="Louisiana">Louisiana</option><option value="Maine">Maine</option><option value="Maryland">Maryland</option><option value="Massachusetts">Massachusetts</option><option value="Minnesota">Minnesota</option><option value="Mississippi">Mississippi</option><option value="Missouri">Missouri</option><option value="Montana">Montana</option><option value="Nebraska">Nebraska</option><option value="Nevada">Nevada</option><option value="New Hampshire">New Hampshire</option><option value="New Jersey">New Jersey</option><option value="New Mexico">New Mexico</option><option value="New York">New York</option><option value="North Carolina">North Carolina</option><option value="North Dakota">North Dakota</option><option value="Ohio">Ohio</option><option value="Oklahoma">Oklahoma</option><option value="Oregon">Oregon</option><option value="Pennsylvania">Pennsylvania</option><option value="Rhode Island">Rhode Island</option><option value="South Carolina">South Carolina</option><option value="South Dakota">South Dakota</option><option value="Tennessee">Tennessee</option><option value="Texas">Texas</option><option value="Utah">Utah</option><option value="Vermont">Vermont</option><option value="Virginia">Virginia</option><option value="Washington">Washington</option><option value="West Virginia">West Virginia</option><option value="Wisconsin">Wisconsin</option><option value="Wyoming">Wyoming</option><option value="American Samoa">American Samoa</option><option value="Guam">Guam</option><option value="Northern Mariana Islands">Northern Mariana Islands</option><option value="Palau">Palau</option><option value="Puerto Rico">Puerto Rico</option><option value="Virgin Islands">Virgin Islands</option><option value="Outside of United States">Outside of United States</option> </select></div></div>
+
+<div class="js-input-wrapper" data-conditional-value="I plan to further my education">
+<input id="after_graguation_plan_radio2" name="data[StudentSurvey][extra][after_graduation_plan][0][field_value]" type="radio" value="I plan to further my education" /> 
+<label for="after_graguation_plan_radio2">I plan to further my education</label>
+	<div class="js-conditional-display js-input-wrapper" style="display:none" data-conditional-value="Other Michigan Institution|Outside of Michigan">
+		 <select class="required" name="data[StudentSurvey][extra][after_graduation_plan][1][child_value]">
+			 <option value="">Please select</option>
+			 <option value="Michigan State University">Michigan State University</option>
+			 <option value="Other Michigan Institution">Other Michigan Institution</option>
+			 <option value="Outside of Michigan">Outside of Michigan</option> 
+		 </select> 
+		 <div class="js-conditional-display" style="display:none">
+			<input class="required" name="i_plan_to_further_my_education_option_other" placeholder="Name of the institution" type="text" value="" />
+		 </div>
+	 </div>
+</div>
+
+<div class="js-input-wrapper" data-conditional-value="I plan to serve in the military"><input id="after_graguation_plan_radio3" name="data[StudentSurvey][extra][after_graduation_plan][0][field_value]" type="radio" value="I plan to serve in the military" /> <label for="after_graguation_plan_radio3">I plan to serve in the military</label>
+<div class="js-conditional-display js-input-wrapper" style="display:none">
+<select class="required" name="data[StudentSurvey][extra][after_graduation_plan][2][child_value]"><option value="">Please select</option><option value="Army">Army</option><option value="Navy">Navy</option><option value="Air Force">Air Force</option><option value="Marines">Marines</option><option value="Coast Guard">Coast Guard</option><option value="Space Force">Space Force</option> </select></div></div>
+
+<div><input id="after_graguation_plan_radio4" name="data[StudentSurvey][extra][after_graduation_plan][0][field_value]" type="radio" value="No plans at this time" /> <label for="after_graguation_plan_radio4">No plans at this time</label></div>
+<div class="js-validation-error"></div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="survey-form-footer continue-btn-wrap"><a class="btn btn-primary border-btn" href="">Cancel</a> <input class="btn btn-primary normalbtn submit-button" name="submit" type="submit" value="Proceed" /></div>
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+</body>
+</html>
