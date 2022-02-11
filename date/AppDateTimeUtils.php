b3872f934661da946e9140ab3f68c8dce918d0b0
Iman <imanali.cse@gmail.com>
2022-02-11 17:51:27 +0600

    date interval

--- date/AppDateTimeUtils.php
+++ date/AppDateTimeUtils.php
@@ -1,7 +1,7 @@
 <?php
 class AppDateTimeUtils
 {
-     public static function firstDayNameOfWeek() : string {
+    public static function firstDayNameOfWeek() : string {
         return date("l", strtotime('this week'));
     }
 
@@ -10,7 +10,11 @@ class AppDateTimeUtils
     }
 
     public static function weekStartDateOfSpecificDate($date) : string {
-        return date("Y-m-d", strtotime("$date this week"));
+        return date("Y-m-d", strtotime($date ." ". self::firstDayNameOfWeek() . " this week"));
+    }
+
+    public static function weekEndDateOfSpecificDate($date) : string {
+        return date("Y-m-d", strtotime($date. " ". self::lastDayNameOfWeek() . " this week"));
     }
 
     public static function getIntervals($start_date, $end_date, $interval_label) : array {
@@ -25,13 +29,9 @@ class AppDateTimeUtils
         return $date_periods;
     }
 
-
-   public static function generateWeeklyDateRanges($start_date, $end_date): array
-    {
-        $week_start_day_name = self::firstDayNameOfWeek();
-        $week_end_day_name = self::lastDayNameOfWeek();
-        $week_start_date_of_start_date =  date("Y-m-d", strtotime("$start_date $week_start_day_name this week"));
-        $week_end_date_of_start_date =  date("Y-m-d", strtotime("$start_date $week_end_day_name this week"));
+    public static function generateWeeklyDateRanges($start_date, $end_date): array {
+        $week_start_date_of_start_date = self::weekStartDateOfSpecificDate($start_date);
+        $week_end_date_of_start_date = self::weekEndDateOfSpecificDate($start_date);
         $initial_date_interval = [];
         if ($start_date !== $week_start_date_of_start_date) {
             $initial_date_interval = [
@@ -41,8 +41,8 @@ class AppDateTimeUtils
             $start_date = date("Y-m-d", strtotime("$week_end_date_of_start_date +1 day"));
         }
 
-        $week_start_date_of_end_date =  date("Y-m-d", strtotime("$end_date $week_start_day_name this week"));
-        $week_end_date_of_end_date =  date("Y-m-d", strtotime("$end_date $week_end_day_name this week"));
+        $week_start_date_of_end_date = self::weekStartDateOfSpecificDate($end_date);
+        $week_end_date_of_end_date = self::weekEndDateOfSpecificDate($end_date);
         $last_date_interval = [];
         if ($end_date !== $week_end_date_of_end_date) {
             $last_date_interval = [
@@ -52,27 +52,53 @@ class AppDateTimeUtils
         }
 
         $date_periods = self::getIntervals($start_date, $end_date, '1 week');
-        $total_period = count($date_periods);
         $date_slot = [];
-        for ($i = 0; $i < $total_period; $i++) {
-            if ($i == $total_period - 1) continue;
+        for ($i = 0; $i < count($date_periods); $i++) {
+            if ($i == count($date_periods) - 1) continue;
             $date_slot[$i]["start_date"] = $date_periods[$i];
             $date_slot[$i]["end_date"] = date('Y-m-d', strtotime($date_periods[$i + 1] . '-1 day'));
         }
 
-        if (!empty($initial_date_interval)) {
-            array_unshift($date_slot, $initial_date_interval);
+        if (!empty($initial_date_interval)) { array_unshift($date_slot, $initial_date_interval); }
+        if (!empty($last_date_interval)) { $date_slot[] = $last_date_interval; }
+        return $date_slot;
+    }
+
+    public static function generateMonthlyDateRanges($start_date, $end_date): array {
+        $initial_date_interval = [];
+        if ($start_date != date('Y-m-01', strtotime($start_date))) {
+            $initial_date_interval = [
+                'start_date' => $start_date,
+                'end_date' => date('Y-m-t', strtotime($start_date)),
+            ];
+            $start_date = date('Y-m-01', strtotime($start_date . ' +1 month'));
         }
-        if (!empty($last_date_interval)) {
-            $date_slot[] = $last_date_interval;
+        $last_date_interval = [];
+        if ($end_date != date('Y-m-t', strtotime($end_date))) {
+            $last_date_interval = [
+                'start_date' => date('Y-m-01', strtotime($end_date)),
+                'end_date' => $end_date,
+            ];
         }
+
+        $date_periods = self::getIntervals($start_date, $end_date, '1 month');
+        $date_slot = [];
+        for ($i = 0; $i < count($date_periods); $i++) {
+            if ($i == count($date_periods) -1) continue;
+            $date_slot[$i]["start_date"] = $date_periods[$i];
+            $date_slot[$i]["end_date"] = date('Y-m-t', strtotime($date_periods[$i]));
+        }
+
+        if (!empty($initial_date_interval)) array_unshift($date_slot, $initial_date_interval);
+        if (!empty($last_date_interval)) $date_slot[] = $last_date_interval;
 
         return $date_slot;
     }
 }
-$start_date = '2021-12-01';
-$end_date = '2022-02-05';
+$start_date = '2021-11-16';
+$end_date = '2022-02-02';
 $date_slot = AppDateTimeUtils::generateWeeklyDateRanges($start_date, $end_date);
+//$date_slot = AppDateTimeUtils::generateMonthlyDateRanges($start_date, $end_date);
 echo "<pre>";
 print_r($date_slot);
 echo "</pre>";
