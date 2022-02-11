c47a35e78a31fcf407a89229bc0ed8559591f67a
Iman <imanali.cse@gmail.com>
2022-02-11 11:02:34 +0600

    date

---
+++ date/characters_date_time.php
@@ -0,0 +1,22 @@
+https://www.php.net/manual/en/datetime.format.php
+
+#Day
+d -	01 to 31
+D - Mon through Sun
+l - Sunday through Saturday
+w - 0 (for Sunday) through 6 (for Saturday)
+N - 1 (for Monday) through 7 (for Sunday)
+j - 1 to 31
+S - st, nd, rd or th. Works well with j
+
+#Time
+H - Hours - 00 through 23
+i - Minutes - 00 to 59
+s - Seconds - 00 through 59
+a - am or pm
+A - AM or PM
+
+#Month
+m - 01 through 12
+M - Jan through Dec
+F - January through December
