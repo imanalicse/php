f85ea73fb0ee74a2f4796bf4070edbc69d3ca2ec
Iman <imanali.cse@gmail.com>
2022-02-18 22:51:28 +0600

    code clean

--- sendgrid-demo/webhook.php
+++ sendgrid-demo/webhook.php
@@ -1,3 +1,6 @@
 <?php
+require_once "functions.php";
+
+waLog("sendgrid_webhook", "Init");
 $webhook_response = @file_get_contents("php://input");
 $webhook_data = json_decode($webhook_response, true); //If json data
