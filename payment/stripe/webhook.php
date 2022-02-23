a97c7cee89fb8c813cdc25c871676ea981be7459
Iman <imanali.cse@gmail.com>
2022-02-23 22:16:27 +0600

    stripe webhook

--- payment/stripe/webhook.php
+++ payment/stripe/webhook.php
@@ -1,9 +1,14 @@
 <?php
 include "include.php";
+use Stripe\Event as WebhookEvent;
 
-waLog("init_stripe_webhook", "stripe_webhook");
+waLog("init_stripe_webhook", "stripe_webhook2");
 $webhook_response = @file_get_contents("php://input");
 $webhook_data = json_decode($webhook_response, true); //If json data
 
 waLog("stripe_webhook_data", "stripe_webhook");
 waLog($webhook_data, "stripe_webhook");
+
+if ($webhook_data['type'] == WebhookEvent::ACCOUNT_UPDATED) {
+}
+
