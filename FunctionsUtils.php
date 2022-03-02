a010dea461ebfd23b18bb927ce9ca793d02a09b3
Iman <imanali.cse@gmail.com>
2022-03-02 11:14:48 +0600

    uuid

--- FunctionsUtils.php
+++ FunctionsUtils.php
@@ -18,4 +18,26 @@ class FunctionsUtils
         parse_str($query_string, $query_string_array);
         return $query_string_array;
     }
+
+    public static function uuid(): string
+    {
+        return sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            // 32 bits for "time_low"
+            random_int(0, 65535),
+            random_int(0, 65535),
+            // 16 bits for "time_mid"
+            random_int(0, 65535),
+            // 12 bits before the 0100 of (version) 4 for "time_hi_and_version"
+            random_int(0, 4095) | 0x4000,
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            random_int(0, 0x3fff) | 0x8000,
+            // 48 bits for "node"
+            random_int(0, 65535),
+            random_int(0, 65535),
+            random_int(0, 65535)
+        );
+    }
 }
