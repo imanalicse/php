3f32f20427725e0b1edd15177922378ea0c2917c
Iman <imanali.cse@gmail.com>
2022-03-02 13:23:11 +0600

    convert to boolean

--- DataTypes.php
+++ DataTypes.php
@@ -11,4 +11,27 @@ class DataTypes
         }
         return $new_value;
     }
+
+    /*
+     * filter_var(true, FILTER_VALIDATE_BOOLEAN); // true
+        filter_var('true', FILTER_VALIDATE_BOOLEAN); // true
+        filter_var(1, FILTER_VALIDATE_BOOLEAN); // true
+        filter_var('1', FILTER_VALIDATE_BOOLEAN); // true
+        filter_var('on', FILTER_VALIDATE_BOOLEAN); // true
+        filter_var('yes', FILTER_VALIDATE_BOOLEAN); // true
+
+        filter_var(false, FILTER_VALIDATE_BOOLEAN); // false
+        filter_var('false', FILTER_VALIDATE_BOOLEAN); // false
+        filter_var(0, FILTER_VALIDATE_BOOLEAN); // false
+        filter_var('0', FILTER_VALIDATE_BOOLEAN); // false
+        filter_var('off', FILTER_VALIDATE_BOOLEAN); // false
+        filter_var('no', FILTER_VALIDATE_BOOLEAN); // false
+        filter_var('abc', FILTER_VALIDATE_BOOLEAN); // false
+        filter_var('', FILTER_VALIDATE_BOOLEAN); // false
+        filter_var(null, FILTER_VALIDATE_BOOLEAN); // false
+     *
+     */
+    public static function convertToBoolean($value) {
+        return filter_var($value, FILTER_VALIDATE_BOOLEAN);
+    }
 }
