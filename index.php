59164b67969e653c7435d302448534a117d0c8f2
Iman <imanali.cse@gmail.com>
2022-03-06 21:08:39 +0600

    google login

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-include "FunctionsUtils.php";
+include "Utils/FunctionsUtils.php";
+include "Utils/EncryptionDecryption.php";
+$text = "Iman Ali";
+$chipper_text = EncryptionDecryption::encrypt($text);
 echo "<pre>";
-print_r(FunctionsUtils::shortenNumberFormat(5005845, 0, true));
+print_r($chipper_text);
+echo "</pre>";
+$decrep_text = EncryptionDecryption::decrypt($chipper_text);
+echo "<pre>";
+print_r($decrep_text);
 echo "</pre>";
