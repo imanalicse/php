dc05afcad3d4a0ddb7a90921419cce4caa26be9c
Iman <imanali.cse@gmail.com>
2022-04-01 14:09:26 +0600

    query builder insert

--- MySQL/QueryBuilder.php
+++ MySQL/QueryBuilder.php
@@ -2,6 +2,7 @@
 namespace App\MySQL;
 
 use App\MySQL\DbConnection;
+use Matrix\Exception;
 
 class QueryBuilder
 {
@@ -18,7 +19,23 @@ class QueryBuilder
         return $rows;
     }
 
-    public function insert($query) {
+    public function insert(string $table_name, array $data) {
+        try {
+            $field_string = implode(',', array_keys($data));
+            $values = array_map(function ($value, $key) {
+                return "'".$value."'";
+            }, array_values($data), array_keys($data));
+            $value_string = implode(',', $values);
+            $sql = "INSERT INTO $table_name ( $field_string ) VALUE ( $value_string )";
+            $result = $this->connection->query($sql);
+        } catch (Exception $exception) {
+            $result =  $exception->getMessage();
+        }
+
+        return $result;
+    }
+
+    public function insertRaw($query) {
         return $this->connection->query($query);
     }
 
