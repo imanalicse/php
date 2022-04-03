1b41d3c9d8aed57a45df73d5b24b5cd42eacce97
Iman <imanali.cse@gmail.com>
2022-04-03 10:37:22 +0600

    slugify

--- Utils/FunctionsUtils.php
+++ Utils/FunctionsUtils.php
@@ -121,4 +121,32 @@ class FunctionsUtils
         }
         return $number_format . $suffix;
     }
+
+    public static function slugify($text, string $divider = '-') : string {
+        // replace non letter or digits by divider
+        $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        // trim
+        $text = trim($text, $divider);
+
+        // remove duplicate divider
+        $text = preg_replace('~-+~', $divider, $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
+        //ref: https://stackoverflow.com/questions/2955251/php-function-to-make-slug-url-string
+    }
+
 }
