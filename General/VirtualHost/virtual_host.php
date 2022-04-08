e279278a02477b538b0f02ddca041137e66b8cd5
Iman <imanali.cse@gmail.com>
2022-04-08 09:33:07 +0600

    virtual host

---
+++ General/VirtualHost/virtual_host.php
@@ -0,0 +1,7 @@
+Wamp location:
+D:\wamp\bin\apache\apache2.4.41\conf\extra\httpd-vhosts.conf
+
+Host entry:
+C:\Windows\System32\drivers\etc\host
+127.0.0.1 olive.doyour.events
+127.0.0.1 test.laraecommerce.com
