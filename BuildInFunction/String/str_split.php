2e00880eff8010dd068ca29332ee9a519350957a
Iman <imanali.cse@gmail.com>
2022-07-21 09:12:56 +0600

    str_split

---
+++ BuildInFunction/String/str_split.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Convert a string to an array
+ * str_split(string $string, int $length = 1): array
+ */
+
+$str = "Hello Friend";
+
+$arr1 = str_split($str);
+$arr2 = str_split($str, 3);
+
+print_r($arr1);
+print_r($arr2);
+
+/*
+Array
+(
+    [0] => H
+    [1] => e
+    [2] => l
+    [3] => l
+    [4] => o
+    [5] =>
+    [6] => F
+    [7] => r
+    [8] => i
+    [9] => e
+    [10] => n
+    [11] => d
+)
+
+Array
+(
+    [0] => Hel
+    [1] => lo
+    [2] => Fri
+    [3] => end
+)
+ */
