18921d5dc6b0803e8193f4205878a70bcfbcb766
Iman <imanali.cse@gmail.com>
2022-08-30 10:09:00 +0600

    regular expression

--- BuildInFunction/PCRE/preg_match_all.php
+++ BuildInFunction/PCRE/preg_match_all.php
@@ -1,7 +1,7 @@
 <?php
 /**
 preg_match_all — Perform a global regular expression match
-preg_match( string $pattern, string $subject, array &$matches = null, int $flags = 0, int $offset = 0): int|false
  */
 
 // TODO
+
