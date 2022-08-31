eab9cb087b41f15efe57fa8464def65f9a1908d5
Iman <imanali.cse@gmail.com>
2022-08-31 11:50:08 +0600

    dirname

---
+++ BuildInFunction/Filesystem/dirname.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * dirname — Returns a parent directory's path
+ *  dirname(string $path, int $levels = 1): string
+ *
+ * path: On Windows, both slash (/) and backslash (\) are used as directory separator character.
+ *In other environments, it is the forward slash (/).
+ *
+ * levels: The number of parent directories to go up. This must be an integer greater than 0.
+ */
+
+echo dirname("/etc/passwd") . "<br>";
+echo dirname("/etc/") . "<br>";
+echo dirname(".") . "<br>";
+echo dirname("C:\\") . "<br>";
+echo dirname("/usr/local/lib", 2) . '<br>';
+/*
+/etc
+/ (or \ on Windows)
+.
+C:\
+/usr
+*/
