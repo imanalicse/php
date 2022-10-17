a63383c2ce2aad48fe1f9c014893f8d4a475a941
Iman <imanali.cse@gmail.com>
2022-10-17 08:51:41 +0600

    curl

--- CurlWork/curl_post_request.php
+++ CurlWork/curl_post_request.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * https://www.php.net/manual/en/function.curl-setopt.php
+ */
 $url = "http://phphub.com/CurlWork/post_receive.php";
 $curl = curl_init();
 $fields = array(
@@ -10,6 +13,7 @@ $fields_string = http_build_query($fields);
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_POST, TRUE);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $fields_string);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // true to return the transfer as a string of the return value of curl_exec() instead of outputting it directly.
 
 $data = curl_exec($curl);
 
