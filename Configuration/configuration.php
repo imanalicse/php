79eadf34db5d05c3528650e6dfc29f7f94ba4e53
Iman <imanali.cse@gmail.com>
2022-12-06 19:17:38 +0600

    configuration

---
+++ Configuration/configuration.php
@@ -0,0 +1,2 @@
+<?php
+echo 'max_input_vars: '. ini_get("max_input_vars") . '<br>';
---
+++ Configuration/htaccess.php
@@ -0,0 +1,7 @@
+<IfModule mod_php7.c>
+#    php_value max_file_uploads 256
+#    php_value upload_max_filesize 256M
+#    php_value post_max_size 256M
+#    php_value memory_limit 256M
+     php_value max_input_vars 10000
+</IfModule>
