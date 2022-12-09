d943f69c8e255a909b6f0af2cb0c8dd69c4ab97f
Iman <imanali.cse@gmail.com>
2022-12-09 12:03:13 +0600

    path info

--- BuildInFunction/Filesystem/pathinfo.php
+++ BuildInFunction/Filesystem/pathinfo.php
@@ -1,1 +1,19 @@
 <?php
+/**
+ * pathinfo — Returns information about a file path
+ */
+
+ $path_info = pathinfo(__FILE__);
+ echo "<pre>";
+ print_r($path_info);
+ echo "</pre>";
+
+ /*
+ Array
+(
+    [dirname] => D:\wamp\www\codehub\php\BuildInFunction\Filesystem
+    [basename] => pathinfo.php
+    [extension] => php
+    [filename] => pathinfo
+)
+  */
