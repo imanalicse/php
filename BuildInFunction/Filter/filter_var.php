e46533e7e62c617b25de07965de878b10b592801
Iman <imanali.cse@gmail.com>
2022-12-09 12:21:39 +0600

    filter var

--- BuildInFunction/Filter/filter_var.php
+++ BuildInFunction/Filter/filter_var.php
@@ -5,13 +5,23 @@
  * Return Values: Returns the filtered data, or false if the filter fails.
  */
 
-var_dump(filter_var('bob@example.com', FILTER_VALIDATE_EMAIL)); // string(15) "bob@example.com"
-var_dump(filter_var('http://example.com', FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED)); // bool(false)
+$email_filter = filter_var('bob@example.com', FILTER_VALIDATE_EMAIL);
+echo "<pre>";
+print_r(var_dump($email_filter)); // string(15) "bob@example.com"
+echo "</pre>";
+$url_filter = filter_var('http://example.com', FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED);
+echo "<pre>";
+print_r(var_dump($url_filter)); // bool(false)
+echo "</pre>";;
 
 // FILTER_SANITIZE_EMAIL
 // https://www.php.net/manual/en/filter.filters.sanitize.php
 // https://www.php.net/manual/en/filter.filters.validate.php
 // https://www.php.net/manual/en/filter.filters.php
 echo "<pre>";
-print_r(filter_var('bob@example.com', FILTER_SANITIZE_EMAIL));
+print_r(var_dump(filter_var(125.8, FILTER_VALIDATE_INT)));
+echo "</pre>";
+
+echo "<pre>";
+print_r(filter_var(125.8, FILTER_SANITIZE_NUMBER_INT));
 echo "</pre>";
