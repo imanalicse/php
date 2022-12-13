e11222e055d4ce4e135a40d4ae45c5c6b3244118
Iman <imanali.cse@gmail.com>
2022-12-13 18:31:48 +0600

    date regex

--- regex/execute_regex.php
+++ regex/execute_regex.php
@@ -1,1 +1,11 @@
 <?php
+/**
+ * PHP Regex to check date is in YYYY-MM-DD format
+ */
+$date="2012-09-12";
+if (preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $date)) {
+    return true;
+}
+else {
+    return false;
+}
