729b4fb1646373e886f46ade8bc1935cb968de46
Iman <imanali.cse@gmail.com>
2022-12-24 22:28:54 +0600

    google auth 2.0

---
+++ Google/GmailAPIAuth2.0.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * https://developers.google.com/gmail/api/reference/rest/v1/users.messages/list
+ *
+ * Auth URL: https://accounts.google.com/o/oauth2/auth
+ * Access Token URL: https://accounts.google.com/o/oauth2/token
+ *
+ * https://developers.google.com/gmail/api/auth/scopes
+ *
+ */
