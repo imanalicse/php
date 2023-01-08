65db9d353436ea16f8cf16fe159657e57cd6fc66
Iman <imanali.cse@gmail.com>
2023-01-08 18:13:52 +0600

    rabbit mq

--- RabbitMQImplementation/tutorial/new_task.php
+++ RabbitMQImplementation/tutorial/new_task.php
@@ -13,7 +13,9 @@ $channel = $connection->channel();
  * To send, we must declare a queue for us to send to; then we can publish a message to the queue:
  * Declaring a queue is idempotent - it will only be created if it doesn't exist already.
  */
-$channel->queue_declare('hello', false, false, false, false);
+// $channel->queue_declare('hello', false, false, false, false);
+// task_queue queue won't be lost even if RabbitMQ restarts by setting durable as true
+$channel->queue_declare('task_queue', false, true, false, false);
 
 // The message content is a byte array, so you can encode whatever you like there.
 
@@ -21,10 +23,16 @@ $data = implode(' ', array_slice($argv, 1));
 if (empty($data)) {
     $data = "Hello World!";
 }
-$msg = new AMQPMessage($data);
 
-$channel->basic_publish($msg, '', 'hello');
+$msg = new AMQPMessage(
+    $data,
+    array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT) // # make message persistent
+);
 
+// $channel->basic_publish($msg, '', 'hello');
+$channel->basic_publish($msg, '', 'task_queue');
+
+echo ' [x] Sent ', $data, "\n";
 
 $channel->close();
 $connection->close();
--- RabbitMQImplementation/tutorial/worker.php
+++ RabbitMQImplementation/tutorial/worker.php
@@ -5,10 +5,10 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 $connection = new AMQPStreamConnection('localhost', 5672, 'iman', 'iman');
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false, false);
+// $channel->queue_declare('hello', false, false, false, false);
+// task_queue queue won't be lost even if RabbitMQ restarts by setting durable as true
+$channel->queue_declare('task_queue', false, true, false, false);
 
- // distribute message fairly
-$channel->basic_qos(null, 1, null);
 
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
@@ -18,7 +18,16 @@ $callback = function ($msg) {
     echo " [x] Done\n";
 };
 
-$channel->basic_consume('hello', '', false, true, false, false, $callback);
+// Fair dispatch:  distribute message fairly
+/**
+ * ->basic_qos method with the prefetch_count = 1 setting tells RabbitMQ not to give more than one message to a worker at a time.
+ * Or, in other words, don't dispatch a new message to a worker until it has processed and acknowledged
+ * the previous one. Instead, it will dispatch it to the next worker that is not still busy.
+ */
+$channel->basic_qos(null, 1, null);
+
+// $channel->basic_consume('hello', '', false, true, false, false, $callback);
+$channel->basic_consume('task_queue', '', false, false, false, false, $callback);
 
 while ($channel->is_open()) {
     // sleep(3);
