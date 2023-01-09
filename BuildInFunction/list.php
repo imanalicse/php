66d5ae8a707c3dd2fa7cbe29ad25340b36a3e16d
Iman <iman@webalive.com.au>
2023-01-09 17:37:40 +0600

    list

---
+++ BuildInFunction/list.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * list — Assign variables as if they were an array
+ */
+
+ $info = array('coffee', 'brown', 'caffeine');
+
+// Listing all the variables
+list($drink, $color, $power) = $info;
+echo "$drink is $color and $power makes it special.\n";
+
+// Listing some of them
+list($drink, , $power) = $info;
+echo "$drink has $power.\n";
