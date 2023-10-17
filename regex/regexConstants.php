8ca5fcf84f88318307277f838234e7fb473e0fa9
Iman <imanali.cse@gmail.com>
2023-10-17 13:05:31 +0600

    Regex constant

--- regex/regexConstants.php
+++ regex/regexConstants.php
@@ -2,15 +2,19 @@
 
 abstract class regexConstants
 {
+    const number_regex = "/^(?:-?\d+|-?\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/";
+    const digit_regex = "/^\d+$/";
+    const only_six_digit_regex = "/[0-9]{6}/";
+    const all_alphabet_regex = "/^[A-Za-z]+$/";
+    const email_regex = "/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/";
+
+    //Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
+    const password_regex = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$#!%*?&])[A-Za-z\d@#$!%*?&]{8,}$/";
+
+    const url_regex = "/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i";
     const website_regex = "/(http:\/\/|https:\/\/)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?/";
     const twitter_regex = "/(?:http:\/\/)?(?:www\.)?twitter\.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w\-]*\/)*([\w\-]*)/";
     const instagram_regex = "/(?:(?:http|https):\/\/)?(?:www.)?(?:instagram.com|instagr.am)\/([A-Za-z0-9-_]+)/im";
     const linkedin_regex = "/(?:http:\/\/)?(?:www\.)?linkedin\.com\/.*$/";
     const facebook_regex = "/^(https?:\/\/)?((w{3}\.)?)facebook.com\/.*/i";
-    const all_alphabet_regex = "/^[A-Za-z]+$/";
-    const all_number_regex = "/^[0-9]+$/";
-    const only_six_number_regex = "/[0-9]{6}/";
-
-    //Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
-    const password_regex = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$#!%*?&])[A-Za-z\d@#$!%*?&]{8,}$/";
 }
