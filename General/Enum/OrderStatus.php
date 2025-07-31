94e680bf8ef55592b9ff0eeb9c528ed49d6abd21
iman <iman@webalive.com.au>
2025-07-31 21:31:53 +1000

    Enum

--- General/Enum/OrderStatus.php
+++ General/Enum/OrderStatus.php
@@ -10,13 +10,24 @@ enum OrderStatus : string
     case DELIVERED = 'delivered';
     case CANCELLED = 'cancelled';
 
+    // Any string can be set here
     public function label(): string {
         return match($this) {
-            self::PENDING => 'Pending',
-            self::PROCESSING => 'Processing',
+            self::PENDING => self::PROCESSING->value,
+            self::PROCESSING => 'The request is underway',
             self::SHIPPED => 'Shipped',
             self::DELIVERED => 'Delivered',
             self::CANCELLED => 'Cancelled',
+        };
+    }
+
+    public function description(): string {
+        return match($this) {
+            self::PENDING => 'Awaiting review, approval',
+            self::PROCESSING => 'The request is underway and actively being worked on',
+            self::SHIPPED => ' The order has been dispatched and is on its way to the delivery address',
+            self::DELIVERED => 'The order has reached the customer and is confirmed as received',
+            self::CANCELLED => 'The order or request has been terminated and will not be processed further',
         };
     }
 
@@ -25,11 +36,26 @@ enum OrderStatus : string
     {
         return array_column(self::cases(), 'value');
     }
+
+    public static function forSelect(): array
+    {
+        return array_combine(
+            array_column(self::cases(), 'value'),
+            array_column(self::cases(), 'name')
+        );
+    }
 }
 
-$label = OrderStatus::PROCESSING->label(); // Processing
+$label = OrderStatus::PROCESSING->label(); // The request is underway
 $value = OrderStatus::PROCESSING->value; // processing
 $name = OrderStatus::PROCESSING->name; // PROCESSING
-echo '<pre>';
-print_r(OrderStatus::values());
-echo '</pre>';
+$description = OrderStatus::PROCESSING->description(); // Awaiting review, approval
+$values = OrderStatus::values();
+
+$enum_from = OrderStatus::from('processing'); //  throws an exception if the value is invalid.
+$enum_from_label = $enum_from->label();  // The request is underway
+
+$enum_from = OrderStatus::tryFrom('processing'); // it is safe: get null instead of an exception
+if ($enum_from) {
+    $enum_from_label = $enum_from->label();  // The request is underway
+}
